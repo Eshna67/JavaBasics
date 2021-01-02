ed16aa89ef75bf1f5d2706661f783f9a7b3f041c
Sharma <eshna.sharma@accenture.com>
2021-01-02 16:23:37 +0530

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Left Shift Operator
+        int a=5;//101
+        int b= a<<2; //10100 --> 20
+
+        System.out.println(b);
+    }
+}
